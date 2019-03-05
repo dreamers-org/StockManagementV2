2e4c46ce29155bc20b4644b3bc226df2ac6d917b
bella2793 <bella2793@gmail.com>
2019-03-05 18:43:36 +0100

    Modificate connection string & varie

--- loft1Mvc/Controllers/ArticoloController.cs
+++ loft1Mvc/Controllers/ArticoloController.cs
@@ -9,144 +9,157 @@ using loft1Mvc.Models;
 
 namespace StockManagement.Controllers
 {
-    public class ArticoloController : Controller
-    {
-        private readonly Loft1Context _context;
+	public class ArticoloController : Controller
+	{
+		private readonly Loft1Context _context;
 
-        public ArticoloController(Loft1Context context)
-        {
-            _context = context;
-        }
+		public ArticoloController(Loft1Context context)
+		{
+			_context = context;
+		}
 
-        // GET: Articolo
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Articoli.ToListAsync());
-        }
+		// GET: Articoli
+		public async Task<IActionResult> Index()
+		{
+			return View(await _context.Articoli.Where(x => x.Annullato == false).ToListAsync());
+		}
 
-        // GET: Articolo/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Articoli/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var articolo = await _context.Articoli
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (articolo == null)
-            {
-                return NotFound();
-            }
+			var articoli = await _context.Articoli
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (articoli == null)
+			{
+				return NotFound();
+			}
 
-            return View(articolo);
-        }
+			return View(articoli);
+		}
 
-        // GET: Articolo/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+		// GET: Articoli/Create
+		public IActionResult Create()
+		{
+			List<string> tipoProdotto = new List<string>();
 
-        // POST: Articolo/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Fornitore,TipoProdotto,Codice,Descrizione,Colore,TrancheConsegna,PrezzoAcquisto,PrezzoVendita,Annullato,attr1,attr2")] Articolo articolo)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(articolo);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(articolo);
-        }
+			tipoProdotto = (from tipo in _context.TipoProdotto
+							select tipo.Tipo).ToList();
 
-        // GET: Articolo/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+			ViewBag.tipiProdotto = tipoProdotto;
+			return View();
+		}
 
-            var articolo = await _context.Articoli.FindAsync(id);
-            if (articolo == null)
-            {
-                return NotFound();
-            }
-            return View(articolo);
-        }
+		// POST: Articoli/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Fornitore,Codice,Descrizione,Colore,Xxxs,Xxs,Xs,S,M,L,Xl,Xxl,Xxxl,Xxxxl,TrancheConsegna,PrezzoAcquisto,PrezzoVendita,TipoProdotto")] Articolo articolo)
+		{
+			//TODO Upload immagine
+			//TODO Ecco come implementare la sessione
+			//_httpContextAccessor.HttpContext.Session.SetString($"{User.Identity.Name}", $"#{id}");
+			//TODO gestire claim e comportamento diversificato: se la fra o un rappresentante arrivano in visualizzazione su questa pagina il comportamento non deve essere lo stesso
+			//OrdiniDaiClienti = await _context.OrdiniDaiClienti.ToListAsync();
+			//OrdiniDaiClienti = (from ordine in OrdiniDaiClienti
+			//					where ordine.Rappresentante == User.Identity.Name
+			//					select ordine).ToList();
+			if (ModelState.IsValid)
+			{
+				//TODO: Controllare al momento dell'inserimento che la coppia codice-colore non sia già stata inserita in precedenza
+				_context.Add(articolo);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(articolo);
+		}
 
-        // POST: Articolo/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Fornitore,TipoProdotto,Codice,Descrizione,Colore,TrancheConsegna,PrezzoAcquisto,PrezzoVendita,Annullato,attr1,attr2")] Articolo articolo)
-        {
-            if (id != articolo.Id)
-            {
-                return NotFound();
-            }
+		// GET: Articoli/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(articolo);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ArticoloExists(articolo.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(articolo);
-        }
+			var articoli = await _context.Articoli.FindAsync(id);
+			if (articoli == null)
+			{
+				return NotFound();
+			}
+			return View(articoli);
+		}
 
-        // GET: Articolo/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// POST: Articoli/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Fornitore,Codice,Descrizione,Colore,Xxxs,Xxs,Xs,S,M,L,Xl,Xxl,Xxxl,Xxxxl,TrancheConsegna,PrezzoAcquisto,PrezzoVendita")] Articolo articolo)
+		{
+			if (id != articolo.Id)
+			{
+				return NotFound();
+			}
 
-            var articolo = await _context.Articoli
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (articolo == null)
-            {
-                return NotFound();
-            }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(articolo);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!ArticoliExists(articolo.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(articolo);
+		}
 
-            return View(articolo);
-        }
+		// GET: Articoli/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-        // POST: Articolo/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var articolo = await _context.Articoli.FindAsync(id);
-            _context.Articoli.Remove(articolo);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+			var articolo = await _context.Articoli
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (articolo == null)
+			{
+				return NotFound();
+			}
 
-        private bool ArticoloExists(int id)
-        {
-            return _context.Articoli.Any(e => e.Id == id);
-        }
-    }
+			return View(articolo);
+		}
+
+		// POST: Articoli/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var articolo = await _context.Articoli.FindAsync(id);
+			_context.Articoli.Remove(articolo);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool ArticoliExists(int id)
+		{
+			return _context.Articoli.Any(e => e.Id == id);
+		}
+	}
 }
