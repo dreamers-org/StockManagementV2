b9ea996ec88fa8078d632523f03c64137038cbac
Luca Bellavia <luca.bellavia.dev@gmail.com>
2019-03-10 19:16:52 +0100

    Aggiunto controller Articolo

---
+++ loft1Mvc/Controllers/ArticoloController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StockManagement.Models;
+
+namespace StockManagement.Controllers
+{
+    public class ArticoloController : Controller
+    {
+        private readonly StockV2Context _context;
+
+        public ArticoloController(StockV2Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Articolo
+        public async Task<IActionResult> Index()
+        {
+            var stockV2Context = _context.Articolo.Include(a => a.IdCollezioneNavigation).Include(a => a.IdFornitoreNavigation).Include(a => a.IdTipoNavigation);
+            return View(await stockV2Context.ToListAsync());
+        }
+
+        // GET: Articolo/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articolo = await _context.Articolo
+                .Include(a => a.IdCollezioneNavigation)
+                .Include(a => a.IdFornitoreNavigation)
+                .Include(a => a.IdTipoNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (articolo == null)
+            {
+                return NotFound();
+            }
+
+            return View(articolo);
+        }
+
+        // GET: Articolo/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCollezione"] = new SelectList(_context.Collezione, "Id", "Nome");
+            ViewData["IdFornitore"] = new SelectList(_context.Fornitore, "Id", "Nome");
+            ViewData["IdTipo"] = new SelectList(_context.Tipo, "Id", "Nome");
+            return View();
+        }
+
+        // POST: Articolo/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Codice,Descrizione,IdFornitore,Colore,Xxs,Xs,S,M,L,Xl,Xxl,TagliaUnica,TrancheConsegna,Genere,IdTipo,Annullato,PrezzoAcquisto,PrezzoVendita,Foto,Video,IdCollezione,DataInserimento,DataModifica,UtenteInserimento,UtenteModifica,Xxxl")] Articolo articolo)
+        {
+            if (ModelState.IsValid)
+            {
+                articolo.Id = Guid.NewGuid();
+                _context.Add(articolo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCollezione"] = new SelectList(_context.Collezione, "Id", "Nome", articolo.IdCollezione);
+            ViewData["IdFornitore"] = new SelectList(_context.Fornitore, "Id", "Nome", articolo.IdFornitore);
+            ViewData["IdTipo"] = new SelectList(_context.Tipo, "Id", "Nome", articolo.IdTipo);
+            return View(articolo);
+        }
+
+        // GET: Articolo/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articolo = await _context.Articolo.FindAsync(id);
+            if (articolo == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCollezione"] = new SelectList(_context.Collezione, "Id", "Nome", articolo.IdCollezione);
+            ViewData["IdFornitore"] = new SelectList(_context.Fornitore, "Id", "Nome", articolo.IdFornitore);
+            ViewData["IdTipo"] = new SelectList(_context.Tipo, "Id", "Nome", articolo.IdTipo);
+            return View(articolo);
+        }
+
+        // POST: Articolo/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Codice,Descrizione,IdFornitore,Colore,Xxs,Xs,S,M,L,Xl,Xxl,TagliaUnica,TrancheConsegna,Genere,IdTipo,Annullato,PrezzoAcquisto,PrezzoVendita,Foto,Video,IdCollezione,DataInserimento,DataModifica,UtenteInserimento,UtenteModifica,Xxxl")] Articolo articolo)
+        {
+            if (id != articolo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(articolo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticoloExists(articolo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCollezione"] = new SelectList(_context.Collezione, "Id", "Nome", articolo.IdCollezione);
+            ViewData["IdFornitore"] = new SelectList(_context.Fornitore, "Id", "Nome", articolo.IdFornitore);
+            ViewData["IdTipo"] = new SelectList(_context.Tipo, "Id", "Nome", articolo.IdTipo);
+            return View(articolo);
+        }
+
+        // GET: Articolo/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articolo = await _context.Articolo
+                .Include(a => a.IdCollezioneNavigation)
+                .Include(a => a.IdFornitoreNavigation)
+                .Include(a => a.IdTipoNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (articolo == null)
+            {
+                return NotFound();
+            }
+
+            return View(articolo);
+        }
+
+        // POST: Articolo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var articolo = await _context.Articolo.FindAsync(id);
+            _context.Articolo.Remove(articolo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArticoloExists(Guid id)
+        {
+            return _context.Articolo.Any(e => e.Id == id);
+        }
+    }
+}
