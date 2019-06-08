1b311fd97fcc315c0c67d9734835e16035c081a6
luca.bellavia.dev <luca.bellavia.dev@gmail.com>
2019-06-08 15:19:59 +0000

    Ordini fornitori Edit

--- loft1Mvc/Controllers/OrdineFornitoreController.cs
+++ loft1Mvc/Controllers/OrdineFornitoreController.cs
@@ -99,6 +99,7 @@ namespace StockManagement.Controllers
                 if (rigaOrdineFornitore == null) return NotFound();
 
                 ViewData["Id"] = new SelectList(_context.Articolo, "Id", "Codice", rigaOrdineFornitore.Id);
+               
                 return View(rigaOrdineFornitore);
             }
             catch (Exception ex)
@@ -114,13 +115,27 @@ namespace StockManagement.Controllers
         {
             try
             {
-                if (id != rigaOrdineFornitore.Id) return NotFound();
+                if (!RigaOrdineFornitoreExists(id)) return NotFound();
 
                 if (ModelState.IsValid)
                 {
                     try
                     {
-                        _context.Update(rigaOrdineFornitore);
+                        RigaOrdineFornitore rigaOrdineFornitoreOld = _context.RigaOrdineFornitore.Where(x => x.Id == id).FirstOrDefault();
+
+                        rigaOrdineFornitoreOld.UtenteModifica = User.Identity.Name;
+                        rigaOrdineFornitoreOld.DataModifica = DateTime.Now;
+                        rigaOrdineFornitoreOld.Xxs = rigaOrdineFornitore.Xxs;
+                        rigaOrdineFornitoreOld.Xs = rigaOrdineFornitore.Xs;
+                        rigaOrdineFornitoreOld.S = rigaOrdineFornitore.S;
+                        rigaOrdineFornitoreOld.M = rigaOrdineFornitore.M;
+                        rigaOrdineFornitoreOld.L = rigaOrdineFornitore.L;
+                        rigaOrdineFornitoreOld.Xl = rigaOrdineFornitore.Xl;
+                        rigaOrdineFornitoreOld.Xxl = rigaOrdineFornitore.Xxl;
+                        rigaOrdineFornitoreOld.Xxxl = rigaOrdineFornitore.Xxxl;
+                        rigaOrdineFornitoreOld.Xxxxl = rigaOrdineFornitore.Xxxxl;
+                        rigaOrdineFornitoreOld.TagliaUnica = rigaOrdineFornitore.TagliaUnica;
+                        _context.Update(rigaOrdineFornitoreOld);
                         await _context.SaveChangesAsync();
                     }
                     catch (DbUpdateConcurrencyException)
@@ -128,10 +143,8 @@ namespace StockManagement.Controllers
                         if (!RigaOrdineFornitoreExists(rigaOrdineFornitore.Id)) return NotFound();
                         throw;
                     }
-                    return RedirectToAction(nameof(Index));
                 }
-                ViewData["Id"] = new SelectList(_context.Articolo, "Id", "Codice", rigaOrdineFornitore.Id);
-                return View(rigaOrdineFornitore);
+                return RedirectToAction(nameof(Index));
             }
             catch (Exception ex)
             {
@@ -140,31 +153,13 @@ namespace StockManagement.Controllers
             }
         }
 
-        public async Task<IActionResult> Delete(Guid? id)
+        public async Task<IActionResult> Delete(Guid id)
         {
             try
             {
                 if (id == null) return NotFound();
+                if (!RigaOrdineFornitoreExists(id)) return NotFound();
 
-                var rigaOrdineFornitore = await _context.RigaOrdineFornitore.FirstOrDefaultAsync(m => m.Id == id);
-
-                if (rigaOrdineFornitore == null) return NotFound();
-
-                return View(rigaOrdineFornitore);
-            }
-            catch (Exception ex)
-            {
-                Utility.GestioneErrori(User.Identity.Name, ex);
-                throw;
-            }
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
-        {
-            try
-            {
                 var rigaOrdineFornitore = await _context.RigaOrdineFornitore.FindAsync(id);
                 _context.RigaOrdineFornitore.Remove(rigaOrdineFornitore);
                 await _context.SaveChangesAsync();
